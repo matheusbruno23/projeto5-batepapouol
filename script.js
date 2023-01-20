1ecac5798e1ae31ff5eebecbd1637164ac24317f
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-01-20 15:27:13 -0300

    envia mensagens no chat

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*globais*/
-let userName, usingName;
+let userName;
 
 /* Reload de mensagens e Status de usuário */
 let reloadStatus, reloadUsers
@@ -7,24 +7,43 @@ let reloadStatus, reloadUsers
 
 function displayChat(){
     userName = prompt("Digite o seu nome de usuário.")
-// registrar usuário - carregar mensagens - carregar usuarios
+// registrar usuário - carregar mensagens 
     registerUser();
     loadMessages();
-  //  loadUsers();
+
+    reloadStatus = setInterval(checkStatus, 5000);
+    setInterval(loadMessages, 3000);
 }
+function errorStatus(error){
+    console.log(error)
+}
+
+function statusOK (response){
+    console.log(response)
+}
+
+function checkStatus(){
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status' , {name:userName});
+    promise.catch(errorStatus)
+    promise.then(statusOK) 
+}
+
+
 
 function sendMessage(){
     const messageText = document.querySelector(".input-message").value;
-    const messageToSent = {from: userName, to: usingName, msg: messageText, type:"message" };
+    const messageToSent = {from: userName, to: "Todos", text: messageText, type:"message" };
     const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messageToSent);
     promise.catch(errorMessage);
     promise.then(messageSent)
     
 }
 function messageSent(response){
+    console.log('ta dando bom');
     console.log(response);
 }
 function errorMessage(error){
+    console.log('ta dando ruim');
     console.log(error)
 }
 
@@ -41,11 +60,23 @@ function displayMessages(response){
     for(let i = 0 ; i <messages.length; i++){
 
         let message = messages[i];
+        const mtype = message.type;
         let templateMessage;
-        if(message.to === "Todos"){ 
+        
+        if(mtype == "status"){ 
             
             templateMessage =
-        `
+            `
+            <li data-test="message" class="left-chat message status">
+                <span class="hour">${message.time}</span>
+                <span class="name">${message.from}</span>
+                <span>:${message.text}</span>
+            </li>
+            `;
+
+    } else if(mtype == "message"){
+            templateMessage = 
+                   `
             <li data-test="message" class="to-all message normal">
             <span class="hour">(${message.time})</span>
             <span class="name">${message.from}</span>
@@ -54,17 +85,7 @@ function displayMessages(response){
             <span>: ${message.text}</span>
             </li>
         `;
-        list.innerHTML += templateMessage;
-    } else if(message.status === "status"){
-            templateMessage = 
-            `
-            <li data-test="message" class="left-chat message status">
-                <span class="hour">${message.time}</span>
-                <span class="name">${message.from}</span>
-                <span>:${message.text}</span>
-            </li>
-            `
-     } else if (message.to !== "Todos"){
+     } else if (mtype == "private_message"){
             templateMessage =
             `
             <li data-test="message" class="private message">
@@ -76,6 +97,8 @@ function displayMessages(response){
             </li>
             `
      }
+
+        list.innerHTML += templateMessage;
     }
 }
 
